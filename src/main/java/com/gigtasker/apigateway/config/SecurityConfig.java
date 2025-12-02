d6af2f86876ed6f35bb33391139c37e999e515c4
Utkarsh Lohani <utkarshlohani88380@gmail.com>
2025-12-02 16:31:45 +0530

    - Added Prometheus Dependency - Moved Security CORS Config to API Gateway

---
+++ src/main/java/com/gigtasker/apigateway/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.gigtasker.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                // 1. Disable CSRF (Fixes 403 Forbidden on Login POST)
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+
+                // 2. Enable CORS (Fixes Preflight errors)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+
+                // 3. Allow EVERYTHING (The Gateway just routes traffic)
+                // Validation happens at the Microservice level (User, Task, etc.)
+                .authorizeExchange(exchange -> exchange
+                        .anyExchange().permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:9090"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
